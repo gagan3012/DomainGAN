80fb15e7e730bb20f5c1d58311ad6706b37c1066
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-23 00:17:03 +0300

    Update generator.py

---
+++ src/generator.py
@@ -0,0 +1,16 @@
+def generator_model():
+    """
+    Generator model:
+    param: noise vector
+    :return: generator model
+    """
+    model = Sequential()
+    model.add(Input(shape=(20,)))
+    model.add(Dense(480, activation='relu'))
+    model.add(ReLU())
+    model.add(decoder_model(480))
+    return model
+
+
+genr = generator_model()
+genr.summary()
