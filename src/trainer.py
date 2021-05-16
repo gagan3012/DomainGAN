237fe829d9fa7b19693657da232fde0705e83568
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-16 10:03:44 -0700

    Update trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -36,6 +36,7 @@ for epoch in range(EPOCHS):
         noise = np.random.normal(0, 1, size=(BATCH_SIZE, 20))  # random latent vectors.
         misleading_targets = np.random.normal(0, 1, size=labels_size)
         gan_history = gan.train_on_batch(noise, misleading_targets, reset_metrics=True, return_dict=True)
+        if (index % 100 == 0):
             print({index: gan_history}, {index: disc_dict})
 print(gan.summary())
 print(genr.summary())
