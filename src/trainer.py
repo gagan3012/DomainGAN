49772aa0c460b3e5fdfa9f723a2e317694c9f8cc
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-16 10:02:40 -0700

    Update trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -1,4 +1,7 @@
             labels = labels_fake
+
+        disc.trainable = True
+        # disc_history = disc.train_on_batch(training_domains, labels,reset_metrics=True,return_dict=True)
         disc_history1 = disc.train_on_batch(normal_domains, labels_real, reset_metrics=True, return_dict=True)
         disc_history2 = disc.train_on_batch(generated_domains, labels_fake, reset_metrics=True, return_dict=True)
         disc_history = np.mean([disc_history1['loss'], disc_history2['loss']])
